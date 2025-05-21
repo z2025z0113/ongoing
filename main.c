8c121d36204c7077f4f2cb774daa99c3b88a60a7
z2025z0113 <z2025z0113@gmail.com>
2025-05-21 17:28:22 +0800

    lops

--- main.c
+++ main.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct expenditureAndIncome{
+	double sumMoney;
+	char time[13]; //="202505211702"; //change to thing happen 
+	char *name;
+	struct expenditureAndIncome *next;
+}ExpenditureIncome;
+
 void AlipayBorrowItCalculate();
+
 int main(){
 	AlipayBorrowItCalculate();
-	//puts("optional alter");
 	return 0;
 }
 
 //borrowIt :the lending business has been deactivated . (principal:30000.) 
 void AlipayBorrowItCalculate(){
+	puts("Alipay borrowIt start:");
+	// update data from file :onlineLoan.txt
 
-	puts("Alipay borrowIt end");
+	puts("Alipay borrowIt end.");
 }
 
