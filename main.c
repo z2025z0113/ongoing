4eb562d601bf4551d6c37942f76b5de2456fad75
z2025z0113 <z2025z0113@gmail.com>
2025-05-21 20:55:59 +0800

    no val

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 typedef struct expenditureAndIncome{
 	double sumMoney;
@@ -14,6 +14,13 @@ void AlipayBorrowItCalculate();
 int main(){
 	puts("program startup:");
 	AlipayBorrowItCalculate();
+	puts("analysis report:");
+	time_t currentTime = time(NULL);
+	struct tm *localTime = localtime(&currentTime);
+	char timeString[100] ;
+	strftime(timeString,sizeof(timeString),"%Y-%m-%d %H:%M:%S",localTime);
+
+	printf("current time:%s\n",timeString);
 	puts("program end.");
 	return 0;
 }
@@ -22,7 +29,7 @@ int main(){
 void AlipayBorrowItCalculate(){
 	puts("Alipay borrowIt start:");
 	// update data from file :onlineLoan.txt
-
+	
 	puts("Alipay borrowIt end.");
 }
 
